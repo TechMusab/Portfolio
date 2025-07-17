2b09a88dd19532ee8e909a146e9b8e9b60c3203f
Musab <joiya4636@gmail.com>
2025-07-17 13:01:14 -0700

    Update HeroModel.jsx

--- src/components/HeroModel.jsx
+++ src/components/HeroModel.jsx
@@ -28,7 +28,7 @@ export default function HeroModel() {
         <group
         scale={isMobile ? 0.5 : isTablet ? 0.7 : 1}
         position={[0,-3.5,0]}
-        rotation={[0, Math.PI / 4, 0]}
+        rotation={[0,-20,0]}
         >
        <Room />
 
